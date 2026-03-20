f4a9d3bd7130f49f791ef582ca3abda27a1e5bb1
woosupar <grademe12@gmail.com>
2026-03-20 14:03:16 +0900

    refactor: 바뀐 라우트 경로 다시 적용

--- config/urls.py
+++ config/urls.py
@@ -10,6 +10,8 @@ urlpatterns = [
 
     # public
     path('api/posts', posts_views.posts_list, name='posts_list'),
+    path('api/posts/<int:event_id>/inquiry', posts_views.event_posts_list, name='event_posts_inquiry'),
+    path('api/posts/<int:event_id>/write', posts_views.event_posts_create, name='event_posts_write'),
     path('api/posts/', include('posts.urls')),
     path('api/comments/', include('posts.comment_urls')),
     path('api/uploads/presign', uploads_views.presign_upload, name='presign_upload'),
